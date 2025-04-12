456b4bc7246f830b83120a3f5ed6978bccbd15bd
SyzygyYuan <yuanzhiyu_163@163.com>
2025-04-13 00:21:15 +0800

    Renew

--- convert.py
+++ convert.py
@@ -2,8 +2,9 @@ import os,json
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
-dic={}
+dic=[]
 for c in get_immediate_subdirectories("."):
-	dic[c]=os.listdir(c)
+	if c!=".git":
+		dic.append({"zi":c,"img":os.listdir(c)})
 with open('data.json', 'w',encoding="utf-8") as f:
     json.dump(dic,f)
